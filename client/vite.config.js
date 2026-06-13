dce19939330f676a8319cd2ac0ca63460592687c
HinduPatrini <hindupatrini16@gmail.com>
2026-06-13 12:26:45 +0530

    added navbar

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // expose to local network so mobile can connect
+    port: 5173,
+  }
 })
+
